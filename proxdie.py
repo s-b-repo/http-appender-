d514d61f9b6d73631c442e4804d292c7aa533355
S.B <30941141+s-b-repo@users.noreply.github.com>
2024-05-11 00:49:42 +0200

    Update proxdie.py

--- proxdie.py
+++ proxdie.py
@@ -1,15 +1,15 @@
 def add_http_and_replace_colon(filename):
     # Open the input file in read mode
-    with open('input.txt', 'r') as file:
+    with open(filename, 'r') as file:
         lines = file.readlines()
 
     # Open a new file to write the modified lines
     with open('output.txt', 'w') as file:
         for line in lines:
-            modified_line = 'http ' + line + line.replace(':', ' ')
+            modified_line = 'http ' + line.replace(':', ' ')
             file.write(modified_line)
 
     print('Lines modified with http and colon replaced with space, saved to output.txt')
 
-# Call the combined function with the file name as argument
+# Call the function with the file name as argument
 add_http_and_replace_colon('input.txt')
