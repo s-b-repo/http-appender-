93b4ec111fea669fbf5c0f4b4d6c402d7d64a3cd
S.B <30941141+s-b-repo@users.noreply.github.com>
2024-05-11 02:00:25 +0200

    Update proxdie.py

--- proxdie.py
+++ proxdie.py
@@ -3,9 +3,12 @@ def modify_txt_file(filename):
         lines = file.readlines()
 
     with open(filename, 'w') as file:
+        modified_lines = []
         for line in lines:
-            modified_line = line.replace('http', 'http ').replace(':', ' ')
-            file.write(modified_line)
+            modified_line = line.strip().replace(':', ' ').replace('http', 'http ')
+            modified_lines.append(modified_line)
+
+        file.write(' '.join(modified_lines))
 
 # Usage
 modify_txt_file('your_file.txt')
